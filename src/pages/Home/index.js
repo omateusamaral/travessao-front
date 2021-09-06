ec27ade931b5f5d925973f33fad3bc145bca2530
omateusamaral <mateus.amaral018@gmail.com>
2021-09-06 15:47:44 -0300

    feat: creating page home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,14 +2,72 @@ import React from 'react';
 
 export function Home() {
   return (
-    <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'>
-      <div className='flex-shrink-0'>
-        <img className='h-12 w-12' src='/img/logo.svg' alt='ChitChat Logo' />
+    <main className='mx-5'>
+      <div className='flex flex-col mb-28'>
+        <h1 className='text-4xl sm:text-4xl text-center lg:text-4xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-2 sm:mt-14 sm:mb-2'>
+          Noticias Recentes
+        </h1>
+        <hr className='border-b-1 w-24 self-center border-black' />
       </div>
-      <div>
-        <div className='text-xl font-medium text-black'>ChitChat</div>
-        <p className='text-gray-500'>You have a new message!</p>
+      <div className='grid grid-cols-3 gap-3  sm:text-xs xl:text-lg lg:text-base'>
+        <div className='row-span-1 mx-1'>
+          aq Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Consequuntur laboriosam, exercitationem mollitia rerum enim corporis
+          facilis recusandae alias quibusdam illo error blanditiis quae
+          doloremque eos deserunt laborum ullam minima autem.
+        </div>
+        <div className=' row-span-2'>
+          aq2 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Accusamus odio expedita asperiores incidunt saepe? Aperiam vel eius
+          incidunt tempore voluptatem assumenda? Suscipit animi velit voluptate
+          cum earum. Recusandae, dicta quia? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Placeat veritatis velit voluptate vitae
+          provident, sint unde perspiciatis possimus totam. Voluptatibus nam
+          eius ipsum harum dignissimos magnam qui maxime alias quae!
+        </div>
+        <div className='row-span-3'>
+          aq 3 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Perspiciatis aliquid sed quia, dolorem doloremque dolores aperiam
+          suscipit vitae cum libero totam quod non eum nesciunt praesentium
+          earum. Aliquam, perferendis at?
+        </div>
+        <div className='row-span-4'>
+          aq 4 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Perspiciatis voluptatem, facere aut sunt, odit obcaecati esse eius
+          assumenda fugiat, impedit asperiores! Voluptatum veniam nostrum quod!
+          Impedit sequi eligendi voluptatibus iusto. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Rem officiis, soluta corrupti harum
+          dolorem illo magni voluptate laboriosam expedita enim vitae totam
+          necessitatibus blanditiis vel quidem, cum incidunt? Quis, eius.
+        </div>
+        <div className='row-span-5'>
+          aq5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          sunt modi animi dolorum adipisci provident ipsa commodi quisquam
+          officia, consequuntur facilis nam reiciendis cumque, dolores omnis
+          aspernatur debitis quis odio.
+        </div>
+        <div className='row-span-6'>
+          aq6 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam
+          commodi sint porro eius eaque dolorem, expedita quaerat! Iste officiis
+          accusantium possimus ad rerum reprehenderit quo nobis. Porro
+          consequatur mollitia deleniti. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Magni, iure ea in rerum consequuntur sapiente a
+          maiores ipsum ratione, saepe voluptates veritatis cumque error qui
+          maxime quia delectus! Labore, ad? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Facilis, ut obcaecati? Voluptatum earum
+          illo tenetur distinctio dignissimos facere! In provident fuga amet
+          similique dignissimos quo voluptatem aspernatur cum, obcaecati minima.
+        </div>
+        <div className='row-span-7'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+          recusandae ullam! Numquam aperiam aliquid nesciunt sunt pariatur.
+          Repellat ipsa ab perferendis inventore vero dolores tempora maxime
+          beatae hic, dolore est. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Numquam nemo libero, placeat fuga mollitia incidunt
+          porro culpa repellat quos aut, facilis nostrum a consequuntur odit!
+          Sint officiis dicta aperiam maiores?
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
