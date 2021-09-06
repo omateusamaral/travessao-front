564068921f6dc62ca563f5c688169ee33eb4de01
omateusamaral <mateus.amaral018@gmail.com>
2021-09-06 16:49:40 -0300

    feat: added mansory layout in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,65 +9,45 @@ export function Home() {
         </h1>
         <hr className='border-b-1 w-24 self-center border-black' />
       </div>
-      <div className='grid grid-cols-3 gap-3  sm:text-xs xl:text-lg lg:text-base'>
-        <div className='row-span-1 mx-1'>
-          aq Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Consequuntur laboriosam, exercitationem mollitia rerum enim corporis
-          facilis recusandae alias quibusdam illo error blanditiis quae
-          doloremque eos deserunt laborum ullam minima autem.
+      <section className='md:masonry-2-col lg:masonry-3-col box-border mx-auto before:box-inherit after:box-inherit'>
+        <div className='break-inside p-8 my-6 bg-gray-100 rounded-lg'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/64626/j-1.jpg' />
+          <p>
+            Não é possível esperar a criatividade chegar. Por isso, este
+            infográfico vai te ajudar a conceber ideias com mais naturalidade.
+          </p>
         </div>
-        <div className=' row-span-2'>
-          aq2 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Accusamus odio expedita asperiores incidunt saepe? Aperiam vel eius
-          incidunt tempore voluptatem assumenda? Suscipit animi velit voluptate
-          cum earum. Recusandae, dicta quia? Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Placeat veritatis velit voluptate vitae
-          provident, sint unde perspiciatis possimus totam. Voluptatibus nam
-          eius ipsum harum dignissimos magnam qui maxime alias quae!
+        <div className='break-inside p-8 my-6 bg-gray-100 rounded-lg'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/64626/j-2.jpg' />
         </div>
-        <div className='row-span-3'>
-          aq 3 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Perspiciatis aliquid sed quia, dolorem doloremque dolores aperiam
-          suscipit vitae cum libero totam quod non eum nesciunt praesentium
-          earum. Aliquam, perferendis at?
+        <div className='break-inside p-8 my-6 bg-gray-100 rounded-lg'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/64626/j-3.jpg' />
+          <p>
+            Não é possível esperar a criatividade chegar. Por isso, este
+            infográfico vai te ajudar a conceber ideias com mais naturalidade.
+          </p>
         </div>
-        <div className='row-span-4'>
-          aq 4 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Perspiciatis voluptatem, facere aut sunt, odit obcaecati esse eius
-          assumenda fugiat, impedit asperiores! Voluptatum veniam nostrum quod!
-          Impedit sequi eligendi voluptatibus iusto. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Rem officiis, soluta corrupti harum
-          dolorem illo magni voluptate laboriosam expedita enim vitae totam
-          necessitatibus blanditiis vel quidem, cum incidunt? Quis, eius.
+        <div className='break-inside p-8 my-6 bg-gray-100 rounded-lg'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/64626/j-4.jpg' />
         </div>
-        <div className='row-span-5'>
-          aq5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-          sunt modi animi dolorum adipisci provident ipsa commodi quisquam
-          officia, consequuntur facilis nam reiciendis cumque, dolores omnis
-          aspernatur debitis quis odio.
+        <div className='break-inside p-8 my-6 bg-gray-100 rounded-lg'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/64626/j-5.jpg' />
         </div>
-        <div className='row-span-6'>
-          aq6 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam
-          commodi sint porro eius eaque dolorem, expedita quaerat! Iste officiis
-          accusantium possimus ad rerum reprehenderit quo nobis. Porro
-          consequatur mollitia deleniti. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Magni, iure ea in rerum consequuntur sapiente a
-          maiores ipsum ratione, saepe voluptates veritatis cumque error qui
-          maxime quia delectus! Labore, ad? Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Facilis, ut obcaecati? Voluptatum earum
-          illo tenetur distinctio dignissimos facere! In provident fuga amet
-          similique dignissimos quo voluptatem aspernatur cum, obcaecati minima.
+        <div className='break-inside p-8 my-6 bg-gray-100 rounded-lg'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/64626/j-6.jpg' />
+          <p>
+            Não é possível esperar a criatividade chegar. Por isso, este
+            infográfico vai te ajudar a conceber ideias com mais naturalidade.
+          </p>
         </div>
-        <div className='row-span-7'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
-          recusandae ullam! Numquam aperiam aliquid nesciunt sunt pariatur.
-          Repellat ipsa ab perferendis inventore vero dolores tempora maxime
-          beatae hic, dolore est. Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Numquam nemo libero, placeat fuga mollitia incidunt
-          porro culpa repellat quos aut, facilis nostrum a consequuntur odit!
-          Sint officiis dicta aperiam maiores?
+        <div className='break-inside p-8 my-6 bg-gray-100 rounded-lg'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/64626/j-1.jpg' />
+          <p>
+            Não é possível esperar a criatividade chegar. Por isso, este
+            infográfico vai te ajudar a conceber ideias com mais naturalidade.
+          </p>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
