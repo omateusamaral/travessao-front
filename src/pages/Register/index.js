4c99bd8a8c70199ebb42d8369a92c85affe09036
omateusamaral <mateus.amaral018@gmail.com>
2021-09-11 20:41:21 -0300

    feat: created register screen

---
+++ src/pages/Register/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+export function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <main className='flex justify-center mt-20'>
+      <div className=' shadow-md py-10  w-3/4 '>
+        <h1 className='text-black leading-normal text-lg font-semibold text-center'>
+          Bem Vindo(a)
+        </h1>
+
+        <form className='w-full'>
+          <Input
+            type='text'
+            placeholder='Seu Nome'
+            value={name}
+            changeValue={(e) => setName(e.target.value)}
+          />
+
+          <Input
+            type='email'
+            placeholder='Seu E-mail'
+            value={email}
+            changeValue={(e) => setEmail(e.target.value)}
+          />
+
+          <Input
+            type='password'
+            placeholder='Senha'
+            value={password}
+            changeValue={(e) => setPassword(e.target.value)}
+          />
+
+          <span className='flex flex-row justify-center'>
+            <button
+              type='submit'
+              className='bg-transparent hover:bg-black text-black font-semibold hover:text-white py-1 px-3 border border-black hover:border-transparent rounded w-2/4  mt-3'
+            >
+              Entrar
+            </button>
+          </span>
+          <div className='flex justify-center w-3/5 mt-2'>
+            <Link to='/login'>
+              <h5 className='text-blue-500'>Já tem uma conta?</h5>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
