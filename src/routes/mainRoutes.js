3373bd2e8ab988e6a5be8528461cc397009e66d4
omateusamaral <mateus.amaral018@gmail.com>
2021-09-11 20:41:28 -0300

    feat:route for register

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import Login from '../pages/Login';
+import { Register } from '../pages/Register';
 import CloseRoutes from './closeRoutes';
 
 export function Routes() {
@@ -9,6 +10,12 @@ export function Routes() {
     <Switch>
       <CloseRoutes path='/' exact component={Home} isCLosed={false} />
       <CloseRoutes path='/login' exact component={Login} isCLosed={false} />
+      <CloseRoutes
+        path='/register'
+        exact
+        component={Register}
+        isCLosed={false}
+      />
     </Switch>
   );
 }
