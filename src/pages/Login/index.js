ee1a8e2fd7875e70e46e14f532fda7e87eb875f8
omateusamaral <mateus.amaral018@gmail.com>
2021-09-11 19:33:54 -0300

    refactor:refactor style in login screen

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import validator from 'validator';
+import { Link } from 'react-router-dom';
 import { MdEmail, MdVpnKey } from 'react-icons/md';
 import { toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
@@ -19,20 +20,20 @@ export default function Login() {
   }
   return (
     <main className='flex justify-center align-middle mt-20'>
-      <div className='rounded-xl shadow-2xl py-10 px-72'>
+      <div className=' shadow-md py-10  w-3/4 '>
         <h1 className='text-black leading-normal text-lg font-semibold text-center'>
           Bem Vindo(a)
         </h1>
 
-        <form onSubmit={handleSubmit}>
-          <span className='flex flex-row justify-center align-middle mb-3'>
+        <form onSubmit={handleSubmit} className='w-full'>
+          <span className='flex flex-row justify-center align-middle mb-3 '>
             <MdEmail
               color='#000'
               size={35}
               className=' border border-r-0 py-2 shadow appearance-none'
             />
             <input
-              className='shadow appearance-none border  rounded-tl-none rounded-bl-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              className='shadow appearance-none border  rounded-tl-none rounded-bl-none rounded w-2/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
               type='email'
               placeholder='E-mail'
               value={email}
@@ -47,19 +48,27 @@ export default function Login() {
               className=' border border-r-0 py-2 shadow appearance-none'
             />
             <input
-              className='shadow appearance-none border  rounded-tl-none rounded-bl-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              className='shadow appearance-none border  rounded-tl-none rounded-bl-none rounded w-2/4  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
               type='password'
               placeholder='Senha'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </span>
-          <button
-            type='submit'
-            className='bg-transparent hover:bg-black text-black font-semibold hover:text-white py-1 px-4 border border-black hover:border-transparent rounded  w-full mt-3'
-          >
-            Entrar
-          </button>
+          <span className='flex flex-row justify-center'>
+            <button
+              type='submit'
+              className='bg-transparent hover:bg-black text-black font-semibold hover:text-white py-1 px-3 border border-black hover:border-transparent rounded w-2/4  mt-3'
+            >
+              Entrar
+            </button>
+          </span>
+
+          <div className='flex justify-center w-3/5 mt-2'>
+            <Link to='/register'>
+              <h5 className='text-blue-500'>Não tem uma conta?</h5>
+            </Link>
+          </div>
         </form>
       </div>
     </main>
