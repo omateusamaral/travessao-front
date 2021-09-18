818ffcf5ac25f2b546a1442f49b6d985e65ddda0
omateusamaral <mateus.amaral018@gmail.com>
2021-09-18 12:41:13 -0300

    refactor: removed unnecessary tags

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -10,10 +10,6 @@ export default function Loading() {
   return (
     <div className='mx-5 '>
       <div className='flex flex-col mb-28'>
-        <h1 className='text-4xl sm:text-4xl text-center lg:text-4xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-2 sm:mt-14 sm:mb-2'>
-          Noticias Recentes
-        </h1>
-        <hr className='border-b-1 w-24 self-center border-black mb-5' />
         <Spin indicator={antIcon} />
       </div>
     </div>
