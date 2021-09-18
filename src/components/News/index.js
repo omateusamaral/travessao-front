a907be114991c72eb29dc679c1c1c05f1d92da25
omateusamaral <mateus.amaral018@gmail.com>
2021-09-18 12:41:37 -0300

    feat: create a component for news

---
+++ src/components/News/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Loading from '../Loading';
+import { Link } from 'react-router-dom';
+export function News({news, loading}) {
+
+  if(loading){
+    return <Loading/>;
+  }
+
+  return (
+    <section className='md:masonry-2-col lg:masonry-3-col box-border  mx-auto before:box-inherit after:box-inherit'>
+      {news.map((recents) => (
+        <div
+          className='break-inside my-6 bg-white rounded-xl shadow-xl hover:shadow-2xl pb-1'
+          key={recents.id}
+        >
+          <p
+            className='text-gray-500 w-full mb-3 text-center no-underline'
+            dangerouslySetInnerHTML={{ __html: recents.resume }}
+          />
+          <Link
+            to={`/${recents.id}/${recents.title_url}`}
+            className='flex justify-center'
+          >
+            <button
+              type='button'
+              className='bg-transparent hover:bg-black text-black font-semibold hover:text-white py-1 px-4 border border-black hover:border-transparent rounded  w-11/12 '
+            >
+                  Visualizar
+            </button>
+          </Link>
+        </div>
+      ))}
+    </section>
+  );
+}
