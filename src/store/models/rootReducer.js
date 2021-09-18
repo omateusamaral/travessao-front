c024947f47447a9e7b02af203cdea6b12d154b85
omateusamaral <mateus.amaral018@gmail.com>
2021-09-18 14:44:56 -0300

    feat: added saga and reducer of news

--- src/store/models/rootReducer.js
+++ src/store/models/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './user/auth/reducer';
 import register from './user/register/reducer';
+import news from './news/find/reducer';
 
 export default combineReducers({
   auth,
   register,
+  news,
 });
--- src/store/models/rootSaga.js
+++ src/store/models/rootSaga.js
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects';
 
 import auth from './user/auth/sagas';
 import register from './user/register/sagas';
+import news from './news/find/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, register]);
+  return yield all([auth, register, news]);
 }
