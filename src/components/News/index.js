e9c393687e02323fcb3514cd6afc37912c9192b7
omateusamaral <mateus.amaral018@gmail.com>
2021-09-18 14:44:11 -0300

    feat: getting news by typing in header

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import Loading from '../Loading';
 import { Link } from 'react-router-dom';
-export function News({news, loading}) {
+import { useSelector } from 'react-redux';
 
-  if(loading){
-    return <Loading/>;
+export function News({ news, loading }) {
+  const foundNews = useSelector((state) => state.news.newsTyped);
+
+  console.log(foundNews);
+  if (!Array.isArray(foundNews)) {
+    news = Object.values(foundNews);
+  }
+  if (loading) {
+    return <Loading />;
   }
 
   return (
     <section className='md:masonry-2-col lg:masonry-3-col box-border  mx-auto before:box-inherit after:box-inherit'>
       {news.map((recents) => (
         <div
-          className='break-inside my-6 bg-white rounded-xl shadow-xl hover:shadow-2xl pb-1'
+          className='break-inside my-6  bg-white rounded-xl shadow-xl hover:shadow-2xl pb-1'
           key={recents.id}
         >
           <p
-            className='text-gray-500 w-full mb-3 text-center no-underline'
+            className='text-gray-500 w-full h-full mb-3 text-center no-underline'
             dangerouslySetInnerHTML={{ __html: recents.resume }}
           />
           <Link
@@ -26,7 +33,7 @@ export function News({news, loading}) {
               type='button'
               className='bg-transparent hover:bg-black text-black font-semibold hover:text-white py-1 px-4 border border-black hover:border-transparent rounded  w-11/12 '
             >
-                  Visualizar
+              Visualizar
             </button>
           </Link>
         </div>
