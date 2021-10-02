35f1910e105485cb456539e6805cbe38001f899e
omateusamaral <mateus.amaral018@gmail.com>
2021-10-02 16:38:28 -0300

    refactor: using slider in home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
 import api from '../../services/axios';
 import { News } from '../../components/News';
-
+import { SliderComponent } from '../../components/SliderComponent';
 export function Home() {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -17,15 +17,18 @@ export function Home() {
     loadNews();
   }, []);
   return (
-    <main className='mx-5'>
-      <div className='flex flex-col mb-28'>
-        <h1 className='text-4xl sm:text-4xl text-center lg:text-4xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-2 sm:mt-14 sm:mb-2'>
-          Notícias Recentes
-        </h1>
-        <hr className='border-b-1 w-24 self-center border-black' />
-      </div>
+    <div className='mt-10'>
+      <SliderComponent />
+      <main className='mx-5'>
+        <div className='flex flex-col mb-28'>
+          <h1 className='text-4xl sm:text-4xl text-center lg:text-4xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-2 sm:mt-14 sm:mb-2'>
+            Notícias Recentes
+          </h1>
+          <hr className='border-b-1 w-24 self-center border-black' />
+        </div>
 
-      <News news={news} loading={loading} />
-    </main>
+        <News news={news} loading={loading} />
+      </main>
+    </div>
   );
 }
